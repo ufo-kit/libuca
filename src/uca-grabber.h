d3221c2e96b4a19fce6dff7af16d859ae05a690b
Matthias Vogelgesang <matthias.vogelgesang@ipe.fzk.de>
2011-02-28 16:03:08 +0100

    Add rpm and deb generation

--- src/uca-grabber.h
+++ src/uca-grabber.h
@@ -19,15 +19,15 @@ typedef uint32_t (*uca_grabber_destroy) (struct uca_grabber_t *grabber);
 /**
  * \brief Set a property
  * \param[in] property_name Name of the property as defined in XXX
- * \return UCA_ERR_PROP_INVALID if property is not supported on the camera or
- * UCA_ERR_PROP_VALUE_OUT_OF_RANGE if value cannot be set.
+ * \return UCA_ERR_PROP_INVALID if property is not supported on the frame
+ * grabber or UCA_ERR_PROP_VALUE_OUT_OF_RANGE if value cannot be set.
  */
 typedef uint32_t (*uca_grabber_set_property) (struct uca_grabber_t *grabber, enum uca_property_ids property, void *data);
 
 /**
  * \brief Set a property
  * \param[in] property_name Name of the property as defined in XXX
- * \return UCA_ERR_PROP_INVALID if property is not supported on the camera
+ * \return UCA_ERR_PROP_INVALID if property is not supported on the frame grabber 
  */
 typedef uint32_t (*uca_grabber_get_property) (struct uca_grabber_t *grabber, enum uca_property_ids property, void *data);
 
