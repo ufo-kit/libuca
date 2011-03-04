ba34fc549873b52f4c78e44c1f71adfc2980bf48
Matthias Vogelgesang <matthias@ipepdvpc2>
2011-03-04 16:28:22 +0100

    Find photon focus library

---
+++ cmake/FindPF.cmake
@@ -0,0 +1,19 @@
+# Try to find libpco.so
+#
+# Defines
+#
+# PCO_FOUND - system has libpco
+# PCO_INCLUDE_DIRS - libpco include directory
+# PCO_LIBRARIES - pco library
+
+find_package(PackageHandleStandardArgs)
+
+find_path(PF_INCLUDE_DIRS libpf/pfcam.h)
+find_library(PF_LIBRARIES libcomdll libmv2_d1280_64)
+
+find_package_handle_standard_args(PF DEFAULT_MSG PF_LIBRARIES PF_INCLUDE_DIRS)
+
+mark_as_advanced(
+    PF_INCLUDE_DIRS
+    PF_LIBRARIES
+)
