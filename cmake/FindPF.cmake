f40cd7548c41c50eb94d7dc7f5b674d5133f6b01
Matthias Vogelgesang <matthias@ipepdvpc2>
2011-03-07 09:01:43 +0100

    Fix libpf not found

--- cmake/FindPF.cmake
+++ cmake/FindPF.cmake
@@ -13,9 +13,9 @@ find_library(PF_LIBRARY_COMDLL NAMES comdll)
 find_library(PF_LIBRARY_MV2 NAMES mv2_d1280_640)
 find_library(PF_LIBRARY_PFCAM NAMES pfcam)
 
-find_package_handle_standard_args(PF DEFAULT_MSG PF_LIBRARIES PF_INCLUDE_DIRS)
-
 set(PF_LIBRARIES ${PF_LIBRARY_MV2} ${PF_LIBRARY_PFCAM} ${PF_LIBRARY_COMDLL})
+
+find_package_handle_standard_args(PF DEFAULT_MSG PF_LIBRARIES PF_INCLUDE_DIRS)
 
 mark_as_advanced(
     PF_INCLUDE_DIRS
