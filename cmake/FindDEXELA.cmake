b8679d1f1aca80f39fbb399151f9b8ac904a0be0
Mihael Koep <koep@schneide.com>
2012-08-28 17:47:23 +0200

    fix detection of libdexela.

--- cmake/FindDEXELA.cmake
+++ cmake/FindDEXELA.cmake
@@ -8,8 +8,8 @@
 
 find_package(PackageHandleStandardArgs)
 
-find_path(DEXELA_INCLUDE_DIRS dexela_api.h PATHS /root/Dexela/include)
-find_library(DEXELA_LIBRARIES dexela PATHS /root/Dexela/dexela-spike)
+find_path(DEXELA_INCLUDE_DIRS dexela_api.h PATHS /usr/include)
+find_library(DEXELA_LIBRARIES dexela)
 
 find_package_handle_standard_args(DEXELA DEFAULT_MSG DEXELA_LIBRARIES DEXELA_INCLUDE_DIRS)
 
