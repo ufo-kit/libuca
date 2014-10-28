1d0a1821e3b9ac325e33b2383fb4baa75227cdc9
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-10-28 10:34:20 +0100

    Remove f'd up tabs

--- plugins/andor/uca-andor-camera.h
+++ plugins/andor/uca-andor-camera.h
@@ -11,14 +11,14 @@
 
 G_BEGIN_DECLS
 
-#define UCA_TYPE_ANDOR_CAMERA			    (uca_andor_camera_get_type())
-#define UCA_ANDOR_CAMERA(obj)			    (G_TYPE_CHECK_INSTANCE_CAST((obj), UCA_TYPE_ANDOR_CAMERA, UcaAndorCamera))
-#define UCA_IS_ANDOR_CAMERA(obj)		    (G_TYPE_CHECK_INSTANCE_TYPE((obj), UCA_TYPE_ANDOR_CAMERA))
-#define UCA_ANDOR_CAMERA_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST((klass), UCA_TYPE_ANDOR_CAMERA, UcaAndorCameraClass))
-#define UCA_IS_ANDOR_CAMERA_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE((klass), UCA_TYPE_ANDOR_CAMERA))
-#define UCA_ANDOR_CAMERA_GET_CLASS(obj)		(G_TYPE_INSTANCE_GET_CLASS((obj), UCA_TYPE_ANDOR_CAMERA, UcaAndorCameraClass))
+#define UCA_TYPE_ANDOR_CAMERA               (uca_andor_camera_get_type())
+#define UCA_ANDOR_CAMERA(obj)               (G_TYPE_CHECK_INSTANCE_CAST((obj), UCA_TYPE_ANDOR_CAMERA, UcaAndorCamera))
+#define UCA_IS_ANDOR_CAMERA(obj)            (G_TYPE_CHECK_INSTANCE_TYPE((obj), UCA_TYPE_ANDOR_CAMERA))
+#define UCA_ANDOR_CAMERA_CLASS(klass)       (G_TYPE_CHECK_CLASS_CAST((klass), UCA_TYPE_ANDOR_CAMERA, UcaAndorCameraClass))
+#define UCA_IS_ANDOR_CAMERA_CLASS(klass)    (G_TYPE_CHECK_CLASS_TYPE((klass), UCA_TYPE_ANDOR_CAMERA))
+#define UCA_ANDOR_CAMERA_GET_CLASS(obj)     (G_TYPE_INSTANCE_GET_CLASS((obj), UCA_TYPE_ANDOR_CAMERA, UcaAndorCameraClass))
 
-#define UCA_ANDOR_CAMERA_ERROR			    uca_andor_camera_error_quark()
+#define UCA_ANDOR_CAMERA_ERROR              uca_andor_camera_error_quark()
 
 typedef enum {
     ANDOR_NOERROR = 0,
@@ -30,10 +30,9 @@ typedef enum {
     UCA_ANDOR_CAMERA_ERROR_ENUM_OUT_OF_RANGE
 } UcaAndorCameraError;
 
-typedef struct _UcaAndorCamera		    UcaAndorCamera;
-typedef struct _UcaAndorCameraClass	    UcaAndorCameraClass;
-typedef struct _UcaAndorCameraPrivate	UcaAndorCameraPrivate;
-
+typedef struct _UcaAndorCamera          UcaAndorCamera;
+typedef struct _UcaAndorCameraClass     UcaAndorCameraClass;
+typedef struct _UcaAndorCameraPrivate   UcaAndorCameraPrivate;
 
 
 struct _UcaAndorCamera {
