ufo-kit/libuca

commit 453701eddba38052a466e91d614fd999b52bca4a

Added check for 'writable during acquisition' to all set_property calls

plugins/file/uca-file-camera.c
@@ -221,7 +221,13 @@ uca_file_camera_set_property (GObject *object, guint property_id, const GValue *
 static void
 uca_file_camera_get_property(GObject *object, guint property_id, GValue *value, GParamSpec *pspec)
 {
-    UcaFileCameraPrivate *priv = UCA_FILE_CAMERA_GET_PRIVATE(object);
+    g_return_if_fail (UCA_IS_FILE_CAMERA (object));
+    UcaFileCameraPrivate *priv = UCA_FILE_CAMERA_GET_PRIVATE (object);
+
+    if (uca_camera_is_recording(UCA_CAMERA(object)) && !uca_camera_is_writable_during_acquisition(UCA_CAMERA(object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
 
     switch (property_id) {
         case PROP_NAME:

plugins/dexela/uca-dexela-camera.c
@@ -264,7 +264,13 @@ static void uca_dexela_camera_get_property(GObject *object, guint property_id, G
 
 static void uca_dexela_camera_set_property(GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
 {
-    UcaDexelaCameraPrivate *priv = UCA_DEXELA_CAMERA_GET_PRIVATE(object);
+    g_return_if_fail (UCA_IS_DEXELA_CAMERA (object));
+    UcaDexelaCameraPrivate *priv = UCA_DEXELA_CAMERA_GET_PRIVATE (object);
+
+    if (uca_camera_is_recording (UCA_CAMERA (object)) && !uca_camera_is_writable_during_acquisition (UCA_CAMERA (object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
 
     switch (property_id) {
         case PROP_EXPOSURE_TIME:

plugins/mock/uca-mock-camera.c
@@ -293,27 +293,32 @@ uca_mock_camera_grab (UcaCamera *camera, gpointer data, GError **error)
 static void
 uca_mock_camera_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
 {
-    g_return_if_fail(UCA_IS_MOCK_CAMERA(object));
-    UcaMockCameraPrivate *priv = UCA_MOCK_CAMERA_GET_PRIVATE(object);
+    g_return_if_fail (UCA_IS_MOCK_CAMERA (object));
+    UcaMockCameraPrivate *priv = UCA_MOCK_CAMERA_GET_PRIVATE (object);
+
+    if (uca_camera_is_recording (UCA_CAMERA (object)) && !uca_camera_is_writable_during_acquisition (UCA_CAMERA (object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
 
     switch (property_id) {
         case PROP_EXPOSURE_TIME:
             priv->exposure_time = g_value_get_double (value);
             break;
         case PROP_ROI_X:
-            priv->roi_x = g_value_get_uint(value);
+            priv->roi_x = g_value_get_uint (value);
             break;
         case PROP_ROI_Y:
-            priv->roi_y = g_value_get_uint(value);
+            priv->roi_y = g_value_get_uint (value);
             break;
         case PROP_ROI_WIDTH:
-            priv->roi_width = g_value_get_uint(value);
+            priv->roi_width = g_value_get_uint (value);
             break;
         case PROP_ROI_HEIGHT:
-            priv->roi_height = g_value_get_uint(value);
+            priv->roi_height = g_value_get_uint (value);
             break;
         default:
-            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
             return;
     }
 }

plugins/pco/uca-pco-camera.c
@@ -687,8 +687,14 @@ uca_pco_camera_grab(UcaCamera *camera, gpointer data, GError **error)
 static void
 uca_pco_camera_set_property(GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
 {
-    UcaPcoCameraPrivate *priv = UCA_PCO_CAMERA_GET_PRIVATE(object);
+    g_return_if_fail (UCA_IS_PCO_CAMERA (object));
+    UcaPcoCameraPrivate *priv = UCA_PCO_CAMERA_GET_PRIVATE (object);
     guint err = PCO_NOERROR;
+
+    if (uca_camera_is_recording (UCA_CAMERA (object)) && !uca_camera_is_writable_during_acquisition (UCA_CAMERA (object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
 
     switch (property_id) {
         case PROP_SENSOR_EXTENDED:
@@ -1781,6 +1787,9 @@ uca_pco_camera_init (UcaPcoCamera *self)
     uca_camera_register_unit (camera, "cooling-point-default", UCA_UNIT_DEGREE_CELSIUS);
     uca_camera_register_unit (camera, "sensor-adcs", UCA_UNIT_COUNT);
     uca_camera_register_unit (camera, "sensor-max-adcs", UCA_UNIT_COUNT);
+
+    uca_camera_set_writable (camera, "exposure-time");
+    uca_camera_set_writable (camera, "frames-per-second");
 }
 
 G_MODULE_EXPORT GType

plugins/pf/uca-pf-camera.c
@@ -208,9 +208,16 @@ uca_pf_camera_trigger(UcaCamera *camera, GError **error)
 static void
 uca_pf_camera_set_property(GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
 {
+    g_return_if_fail (UCA_IS_PF_CAMERA (object));
+
+    if (uca_camera_is_recording (UCA_CAMERA (object)) && !uca_camera_is_writable_during_acquisition (UCA_CAMERA (object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
+
     switch (property_id) {
         default:
-            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
             return;
     }
 }

plugins/pylon/uca-pylon-camera.c
@@ -135,8 +135,14 @@ static gboolean uca_pylon_camera_grab(UcaCamera *camera, gpointer data, GError *
 
 static void uca_pylon_camera_set_property(GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
 {
-    UcaPylonCameraPrivate *priv = UCA_PYLON_CAMERA_GET_PRIVATE(object);
+    g_return_if_fail (UCA_IS_PYLON_CAMERA (object));
+    UcaPylonCameraPrivate *priv = UCA_PYLON_CAMERA_GET_PRIVATE (object);
     GError* error = NULL;
+
+    if (uca_camera_is_recording (UCA_CAMERA (object)) && !uca_camera_is_writable_during_acquisition (UCA_CAMERA (object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
 
     switch (property_id) {
     case PROP_SENSOR_HORIZONTAL_BINNING:

plugins/ufo/uca-ufo-camera.c
@@ -424,7 +424,13 @@ total_readout_time (UcaUfoCamera *camera)
 static void
 uca_ufo_camera_set_property(GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
 {
-    UcaUfoCameraPrivate *priv = UCA_UFO_CAMERA_GET_PRIVATE(object);
+    g_return_if_fail (UCA_IS_UFO_CAMERA (object));
+    UcaUfoCameraPrivate *priv = UCA_UFO_CAMERA_GET_PRIVATE (object);
+
+    if (uca_camera_is_recording (UCA_CAMERA (object)) && !uca_camera_is_writable_during_acquisition (UCA_CAMERA (object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
 
     switch (property_id) {
         case PROP_EXPOSURE_TIME:

commit a26120e69e26186454becc7658859224a4a3de2c

Fix wrong place of write check caused by 453701edd

plugins/file/uca-file-camera.c
@@ -205,6 +205,12 @@ uca_file_camera_set_property (GObject *object, guint property_id, const GValue *
     g_return_if_fail(UCA_IS_FILE_CAMERA(object));
     UcaFileCameraPrivate *priv = UCA_FILE_CAMERA_GET_PRIVATE(object);
 
+    if (uca_camera_is_recording (UCA_CAMERA(object)) &&
+        !uca_camera_is_writable_during_acquisition (UCA_CAMERA (object), pspec->name)) {
+        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
+        return;
+    }
+
     switch (property_id) {
         case PROP_PATH:
             g_free (priv->path);
@@ -226,11 +232,6 @@ uca_file_camera_get_property(GObject *object, guint property_id, GValue *value, 
 {
     g_return_if_fail (UCA_IS_FILE_CAMERA (object));
     UcaFileCameraPrivate *priv = UCA_FILE_CAMERA_GET_PRIVATE (object);
-
-    if (uca_camera_is_recording(UCA_CAMERA(object)) && !uca_camera_is_writable_during_acquisition(UCA_CAMERA(object), pspec->name)) {
-        g_warning ("Property '%s' cant be changed during acquisition", pspec->name);
-        return;
-    }
 
     switch (property_id) {
         case PROP_NAME: